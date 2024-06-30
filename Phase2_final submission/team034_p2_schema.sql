e9cd9758b69508ec68cd8c4e4b6ccb23f31c9565
Yan, Qiyan <qyan64@gatech.edu>
2024-06-30 13:16:56 -0500

    Add files via upload

---
+++ Phase2_final submission/team034_p2_schema.sql
@@ -0,0 +1,113 @@
+CREATE TABLE District (
+    DistrictNumber INT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE City (
+    CityName VARCHAR(255) NOT NULL,
+    State VARCHAR(255) NOT NULL,
+    Population INT NULL,
+    PRIMARY KEY (CityName)
+);
+
+CREATE TABLE Store (
+    StoreNumber INT PRIMARY KEY NOT NULL,
+    PhoneNumber VARCHAR(255) NULL,
+    DistrictNumber INT NOT NULL,
+    CityName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (DistrictNumber ) REFERENCES District(DistrictNumber),
+    FOREIGN KEY (CityName) REFERENCES City(CityName),
+    UNIQUE KEY PhoneNumber(PhoneNumber)
+);
+
+
+CREATE TABLE Manufacturer (
+    ManufacturerName VARCHAR(255) PRIMARY KEY NOT NULL
+);
+
+
+CREATE TABLE Product (
+    PID INT(50) PRIMARY KEY NOT NULL,
+    ProductName VARCHAR(255) NULL,
+    ManufacturerName VARCHAR(255) NOT NULL,
+    RetailPrice DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (ManufacturerName) REFERENCES Manufacturer(ManufacturerName)
+);
+
+
+CREATE TABLE Category (
+    CategoryName VARCHAR(255) PRIMARY KEY NOT NULL
+);
+
+
+CREATE TABLE Assignto (
+    PID INT(50) NOT NULL,
+    CategoryName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (PID) REFERENCES Product(PID),
+    FOREIGN KEY (CategoryName) REFERENCES Category(CategoryName)
+);
+ 
+
+CREATE TABLE BusinessDay ( 
+    BusinessDate DATE PRIMARY KEY NOT NULL
+);
+
+
+CREATE TABLE Discount (
+    PID INT(50) NOT NULL,
+    BusinessDate DATE NOT NULL,
+    DiscountPrice DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (PID) REFERENCES Product(PID)
+);
+
+
+CREATE TABLE Sells (
+    StoreNumber INT NOT NULL,
+    PID INT(50) NOT NULL,
+    QuantitySold INT (10) NOT NULL,
+    DateSold DATE NOT NULL,
+    FOREIGN KEY (StoreNumber) REFERENCES Store(StoreNumber),
+    FOREIGN KEY (PID) REFERENCES Product(PID)
+);
+
+
+CREATE TABLE Holidays (
+    BusinessDate DATE PRIMARY KEY NOT NULL,
+    HolidayName VARCHAR(255) NOT NULL,
+    UNIQUE KEY HolidayName(HolidayName)
+);
+
+
+CREATE TABLE User (
+    EmployeeID VARCHAR(7) PRIMARY KEY NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    LastFourSSN INT NOT NULL,
+    AccessToAuditLog BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE Created (
+    EmployeeID VARCHAR(7) NOT NULL,
+    BusinessDate DATE NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES User(EmployeeID),
+    FOREIGN KEY (BusinessDate) REFERENCES BusinessDay(BusinessDate)
+);
+
+CREATE TABLE CanAccess (
+    EmployeeID VARCHAR(7) NOT NULL,
+    DistrictNumber INT (10) NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES User(EmployeeID),
+    FOREIGN KEY (DistrictNumber) REFERENCES District(DistrictNumber)
+);
+
+CREATE TABLE Report (
+    ReportName VARCHAR(255) PRIMARY KEY NOT NULL
+);
+
+
+CREATE TABLE AuditLogEntry (
+    EmployeeID VARCHAR(7) NOT NULL,
+    Timestamp TIMESTAMP NOT NULL,
+    ReportName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES User(EmployeeID)
+);
+
