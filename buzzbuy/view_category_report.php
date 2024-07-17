694a15e56dc568b38588a3ad83a584f894f152d4
Qiyan Yan <qyan64@gatech.edu>
2024-07-16 21:19:56 -0500

    add the latest view category report, view manufacturer's product report with it's drilldowns

---
+++ buzzbuy/view_category_report.php
@@ -0,0 +1,109 @@
+<?php
+
+include('lib/common.php');
+// written by Team 34
+
+if (!isset($_SESSION['employeeid'])) {
+	header('Location: login.php');
+	exit();
+}
+
+
+// just to display a signed-in user's information 
+$query = "SELECT firstname, lastname " .
+		 "FROM User " .
+		 "WHERE User.employeeid='{$_SESSION['employeeid']}'";
+
+$result = mysqli_query($db, $query);
+include('lib/show_queries.php');
+ 
+if ( !is_bool($result) && (mysqli_num_rows($result) > 0) ) {
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+} else {
+    array_push($error_msg,  "Query ERROR: Failed to get User information...<br>" . __FILE__ ." line:". __LINE__ );
+}
+	
+?>
+
+<?php include("lib/header.php"); ?>
+		<title>BuzzBuy View Category</title>
+	</head>
+	
+	<body>
+        <div id="main_container">
+		    <?php include("lib/menu.php"); ?>
+            
+			<div class="center_content">
+				<div class="center_left">
+					<div class="title_name"><?php print $user_name; ?></div>          
+					
+					<div class="features">   	
+						<div class="profile_section">
+                        	<div class="subtitle">View Category</div>   
+							<table>
+								<tr>
+									<td class="heading">Category Name</td>
+									<td class="heading">Total Products</td>
+									<td class="heading">Total Manufacturers</td>
+									<td class="heading">Average Retail Prices</td>
+								</tr>
+																
+								<?php								
+                                    $query = "SELECT assignto.CategoryName, 
+                                             COUNT(DISTINCT product.PID) AS TotalProducts, 
+                                             COUNT(DISTINCT product.ManufacturerName) AS TotalManufacturers, 
+                                             AVG(product.RetailPrice) AS AvgRetailPrice 
+											 FROM assignto 
+											 JOIN product ON assignto.PID = product.PID 
+											 GROUP BY assignto.CategoryName 
+											 ORDER BY assignto.CategoryName ASC";
+                                             
+                                    $result = mysqli_query($db, $query);
+                                     if (!empty($result) && (mysqli_num_rows($result) == 0) ) {
+                                         array_push($error_msg,  "SELECT ERROR: find Category <br>" . __FILE__ ." line:". __LINE__ );
+                                    }
+                                    
+                                    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                                        print "<tr>";
+                                        print "<td>{$row['CategoryName']}</td>";
+										print "<td>{$row['TotalProducts']}</td>";
+										print "<td>{$row['TotalManufacturers']}</td>";
+										print "<td>{$row['AvgRetailPrice']}</td>";
+                                        print "</tr>";							
+                                    }									
+                                ?>
+							</table>						
+						</div>	
+					 </div> 
+				</div> 
+                
+                <?php include("lib/error.php"); ?>
+                    
+				<div class="clear"></div> 
+			</div>    
+
+               <?php include("lib/footer.php"); ?>
+		 
+		</div>
+				<!-- add a log entry -->
+		<?php 
+			$report_name = "Report: View Category";
+			$timestamp = date("Y-m-d H:i:s");
+	
+			// Escape variables for safety
+			$escaped_employeeid = mysqli_real_escape_string($db, $_SESSION['employeeid']);
+			$escaped_timestamp = mysqli_real_escape_string($db, $timestamp);
+			$escaped_report_name = mysqli_real_escape_string($db, $report_name);
+
+			$audit_query = "INSERT INTO AuditLogEntry (employeeid, timestamp, reportName) VALUES ('{$escaped_employeeid}', '{$escaped_timestamp}', '{$escaped_report_name}')";
+
+			// Execute the query
+			$result = mysqli_query($db, $audit_query);
+    		include('lib/show_queries.php');
+
+			if ($result === False) {
+				array_push($error_msg, "Error: Failed to add Audit Log Entry: " . mysqli_error($db));
+			}
+		?>
+	</body>
+</html>
---
+++ buzzbuy/view_product_report.php
@@ -0,0 +1,109 @@
+<?php
+
+include('lib/common.php');
+// written by Team 34
+
+if (!isset($_SESSION['employeeid'])) {
+	header('Location: login.php');
+	exit();
+}
+
+// just to display a signed-in user's information 
+$query = "SELECT firstname, lastname " .
+		 "FROM User " .
+		 "WHERE User.employeeid='{$_SESSION['employeeid']}'";
+
+$result = mysqli_query($db, $query);
+include('lib/show_queries.php');
+ 
+if ( !is_bool($result) && (mysqli_num_rows($result) > 0) ) {
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+} else {
+    array_push($error_msg,  "Query ERROR: Failed to get User information...<br>" . __FILE__ ." line:". __LINE__ );
+}
+	
+?>
+
+<?php include("lib/header.php"); ?>
+		<title>BuzzBuy View Manufacturer's Product</title>
+	</head>
+	
+	<body>
+        <div id="main_container">
+		    <?php include("lib/menu.php"); ?>
+            
+			<div class="center_content">
+				<div class="center_left">
+					<div class="title_name"><?php print $user_name; ?></div>          
+					
+					<div class="features">   	
+						<div class="profile_section">
+                        	<div class="subtitle">View Manufacturer's Product</div>   
+							<table>
+								<tr>
+									<td class="heading">Manufacturer's Name</td>
+									<td class="heading">Total Number of Products</td>
+									<td class="heading">Average Retail Price</td>
+									<td class="heading">Minimum Retail Price</td>
+									<td class="heading">Maximum Retail Price</td>
+								</tr>
+																
+								<?php								
+                                    $query = "SELECT Manufacturer.ManufacturerName, COUNT(Product.PID) AS ProductCount, AVG(Product.RetailPrice) AS AveragePrice, MAX(Product.RetailPrice) AS MaxPrice, MIN(Product.RetailPrice) AS MinPrice
+											 FROM Manufacturer JOIN Product ON Product.ManufacturerName = Manufacturer.ManufacturerName
+											 GROUP BY Manufacturer.ManufacturerName
+											 ORDER BY AveragePrice DESC
+											 LIMIT 100" ;
+                                             
+                                    $result = mysqli_query($db, $query);
+                                     if (!empty($result) && (mysqli_num_rows($result) == 0) ) {
+                                         array_push($error_msg,  "SELECT ERROR: find Friendship <br>" . __FILE__ ." line:". __LINE__ );
+                                    }
+                                    
+                                    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+										$manufacturerName = urlencode($row['ManufacturerName']);
+                                        print "<tr>";
+                                        print "<td>{$row['ManufacturerName']}</td>";
+                                        print "<td>{$row['ProductCount']}</td>";
+                                        print "<td>{$row['AveragePrice']}</td>";
+										print "<td>{$row['MaxPrice']}</td>";
+										print "<td>{$row['MinPrice']}</td>";
+										print "<td><a href='manufacturer_details.php?manufacturer=$manufacturerName'>View Details</a></td>";
+                                        print "</tr>";							
+                                    }									
+                                ?>
+							</table>						
+						</div>	
+					 </div> 
+				</div> 
+                
+                <?php include("lib/error.php"); ?>
+                    
+				<div class="clear"></div> 
+			</div>    
+
+               <?php include("lib/footer.php"); ?>
+		 
+		</div>
+		<!-- add a log entry -->
+		<?php 
+			$report_name = "Report: View Manufacturer's Product";
+			$timestamp = date("Y-m-d H:i:s");
+	
+			// Escape variables for safety
+			$escaped_employeeid = mysqli_real_escape_string($db, $_SESSION['employeeid']);
+			$escaped_timestamp = mysqli_real_escape_string($db, $timestamp);
+			$escaped_report_name = mysqli_real_escape_string($db, $report_name);
+
+			$audit_query = "INSERT INTO AuditLogEntry (employeeid, timestamp, reportName) VALUES ('{$escaped_employeeid}', '{$escaped_timestamp}', '{$escaped_report_name}')";
+
+			// Execute the query
+			$result = mysqli_query($db, $audit_query);
+    		include('lib/show_queries.php');
+
+			if ($result === False) {
+				array_push($error_msg, "Error: Failed to add Audit Log Entry: " . mysqli_error($db));
+			}
+		?>
+	</body>
+</html>
